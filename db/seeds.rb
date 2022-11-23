e8b5f32495a093b27163f7904ad884eaba96cc11
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-23 08:03:44 +0300

    Added react client

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(title:"You", year:"2022",length:"",director:"kim",description:"whatever",poster_url:"scf",category:"horror",discount:true)
+Movie.create(title:"Me", year:"2022",length:"",director:"kim",description:"whatever",poster_url:"scf",category:"horror",discount:true)
+Movie.create(title:"Us", year:"2022",length:"",director:"kim",description:"whatever",poster_url:"scf",category:"horror",discount:true)
+Movie.create(title:"Them", year:"2022",length:"",director:"kim",description:"whatever",poster_url:"scf",category:"horror",discount:true)
+Movie.create(title:"They", year:"2022",length:"",director:"kim",description:"whatever",poster_url:"scf",category:"horror",discount:true)
+
